3f20a91dc35a74ec0ad9fb50925b7fe172c46583
RomanPivovaroff <romain.pivovaroff@yandex.ru>
2025-03-12 18:11:34 +0300

    bd-1 script fix

--- DB/LAB1/script.sql
+++ DB/LAB1/script.sql
@@ -7,20 +7,7 @@ CREATE TABLE Human (
  name TEXT NOT NULL,
  profession TEXT NOT NULL,
  age INT NOT NULL,
-);
-CREATE TABLE Sensation (
- id SERIAL PRIMARY KEY,
- fall_derection DERECTION NOT NULL,
- emotion TEXT NOT NULL,
- now_time TIME NOT NULL,
- now_date DATE NOT NULL,
- location_id INT REFERENCES Location (id) ON DELETE SET NULL,
- visible_light_id INT REFERENCES Light (id) ON DELETE SET NULL
-);
-CREATE TABLE Light (
- id SERIAL PRIMARY KEY,
- distance_to_light FLOAT,
- brightness INT NOT NULL
+ CHECK (name SIMILAR TO '[A-Za-zА-Яа-я]+')
 );
 CREATE TABLE World (
  id SERIAL PRIMARY KEY,
@@ -32,7 +19,21 @@ CREATE TABLE Location (
  name TEXT NOT NULL,
  terrain TERRAIN NOT NULL,
  environment ENVORONMENT NOT NULL,
- world_id INT REFERENCES World (id) ON DELETE CASCADE,
+ world_id INT REFERENCES World (id) ON DELETE CASCADE
+);
+CREATE TABLE Light (
+ id SERIAL PRIMARY KEY,
+ distance_to_light FLOAT,
+ brightness INT NOT NULL
+);
+CREATE TABLE Sensation (
+ id SERIAL PRIMARY KEY,
+ fall_derection DERECTION NOT NULL,
+ emotion TEXT NOT NULL,
+ now_time TIME NOT NULL,
+ now_date DATE NOT NULL,
+ location_id INT REFERENCES Location (id) ON DELETE SET NULL,
+ visible_light_id INT REFERENCES Light (id) ON DELETE SET NULL
 );
 CREATE TABLE Portal (
  id SERIAL PRIMARY KEY,
@@ -51,25 +52,25 @@ INSERT INTO World (name, known) VALUES
  (NULL, FALSE),
  ('Earth', TRUE);
  INSERT INTO Location (name, terrain, environment, world_id) VALUES
- ('Tunnel', 'other', 'normal', 1),
- ('House', 'other', 'good', 3);
- ('unknown plain', 'plain', 'mormal', 2);
+ ('Tunnel', 'other', 'normal', (SELECT id FROM World WHERE name = 'Japet')),
+ ('House', 'other', 'well', (SELECT id FROM World WHERE name = 'Earth')),
+ ('unknown plain', 'plain', 'normal', 2);
 INSERT INTO Human (name, profession, age) VALUES
  ('Boymen', 'world traveler', 30),
  ('Roman', 'programmer', 19);
 INSERT INTO Light (distance_to_light, brightness) VALUES
  (NULL, 1500),
- (, 369000000),
+ (149597871, 369000000),
  (1, 1500),
  (0.5, 1500),
  (0.01, 1500);
-INSERT INTO Sensation (fall_derection, emotion, now_time, now_date location_id, visible_light_id) VALUES
- ('not fall', 'unhappy', 7:00:00, 2025-03-11, 2, 2),
- ('down', 'scared', 12:35:00, 1989-01-23, 1, 1),
- ('down', 'scared', 12:35:05, 1989-01-23, 1, 3),
- ('up', 'scared', 12:35:10, 1989-01-23, 1, 4),
- ('up', 'scared', 12:35:15, 1989-01-23, 1, 5),
- ('not fall', 'scared', 12:35:20, 1989-01-23, 3, NULL);
+INSERT INTO Sensation (fall_derection, emotion, now_time, now_date, location_id, visible_light_id) VALUES
+ ('not fall', 'unhappy', '7:00:00', '2025-03-11', 2, 2),
+ ('down', 'scared', '12:35:00', '1989-01-23', 1, 1),
+ ('down', 'scared', '12:35:05', '1989-01-23', 1, 3),
+ ('up', 'scared', '12:35:10', '1989-01-23', 1, 4),
+ ('up', 'scared', '12:35:15', '1989-01-23', 1, 5),
+ ('not fall', 'scared', '12:35:20', '1989-01-23', 3, NULL);
 INSERT INTO Portal (width, arrival_world_id, departure_world_id, location_id) VALUES
  (3.5, 2, 1, 1);
 INSERT INTO Sensation_to_Human (sensation_id, human_id) VALUES
